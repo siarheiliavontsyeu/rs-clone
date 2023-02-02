c973a5f0acc2c84c8f64e1b1527d1566eb504336
Sansrona <kingJaM00@yandex.ru>
2023-02-02 20:41:33 +0600

    feat: create vue-app, add pinia-store usage, remove welcome component

---
+++ client/src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>Страница about</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter'
+const counter = useCounterStore();
+</script>
+
+<template>
+  <main>Пустая страница</main>
+  <div>{{ counter.count }}</div>
+  <div>{{ counter.doubleCount }}</div>
+  <button @click="counter.increment">Увеличить</button>
+  <button @click="counter.decrement">Уменшить</button>
+</template>
