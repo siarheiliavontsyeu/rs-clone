6c03a478c77df75ad6cfab0955dbc4be179710fb
Liavontsyeu Siarhei <leontevsi@gmail.com>
2023-02-02 16:51:27 +0300

    feat: add app, server, config

---
+++ server/src/app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const swaggerUI = require('swagger-ui-express');
+const path = require('path');
+const YAML = require('yamljs');
+const createError = require('http-errors');
+const { OK, NOT_FOUND } = require('http-status-codes');
+const morgan = require('morgan');
+const winston = require('./common/logging');
+const errorHandler = require('./errors/errorHandler');
+const checkToken = require('./common/check-token');
+require('express-async-errors');
+
+const loginRouter = require('./resources/login/login.router');
+const userRouter = require('./resources/users/user.router');
+const watchHistoryRouter = require('./resources/watch-history/watch-history.router');
+
+const app = express();
+app.disable('x-powered-by');
+const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
+
+app.use(express.json());
+app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use(checkToken);
+
+app.use(
+  morgan(
+    ':method :status :url :query Body :body size :res[content-length] - :response-time ms',
+    {
+      stream: winston.stream
+    }
+  )
+);
+
+app.use('/', (req, res, next) => {
+  if (req.originalUrl === '/') {
+    res.status(OK).send('Service is running!');
+    return;
+  }
+  next();
+});
+
+app.use('/login', loginRouter);
+app.use('/users', userRouter);
+app.use('/watch-history', watchHistoryRouter);
+
+app.use((req, res, next) => next(createError(NOT_FOUND)));
+
+app.use(errorHandler);
+
+// throw Error('Oops!');
+// Promise.reject(Error('Oops!'));
+
+module.exports = app;
---
+++ server/src/server.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const { PORT, MONGO_CONNECTION_STRING } = require('./common/config');
+const logger = require('./common/logging');
+const app = require('./app');
+// const { create } = require('./resources/users/user.service');
+
+// const MOCK_USER = {
+//   login: 'admin',
+//   password: 'admin',
+//   subscription: 'premium'
+// };
+
+mongoose.connect(MONGO_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+db.on('error', () => logger.error('MongoDB connection fail:')).once(
+  'open',
+  async () => {
+    logger.info('Successfully connect to MongoDB');
+    // await create(MOCK_USER);
+    logger.info('Successfully create mock admin user');
+    app.listen(PORT, () =>
+      logger.info(`App is running on http://localhost:${PORT}`)
+    );
+  }
+);
