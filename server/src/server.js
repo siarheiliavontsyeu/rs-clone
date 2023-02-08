2f4b150e503ea4f9226e5eda7303ff84751a5a81
Liavontsyeu Siarhei <leontevsi@gmail.com>
2023-02-09 00:04:57 +0300

    feat: add ssl cert

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-sync */
 const mongoose = require('mongoose');
 const { PORT, MONGO_CONNECTION_STRING } = require('./common/config');
 const logger = require('./common/logging');
+const https = require('https');
+const fs = require('fs');
 const app = require('./app');
 // const { create } = require('./resources/users/user.service');
 
@@ -9,6 +12,12 @@ const app = require('./app');
 //   password: 'admin',
 //   subscription: 'premium'
 // };
+
+const sslOptions = {
+  key: fs.readFileSync('/home/lsi/back/src/key.pem'),
+  cert: fs.readFileSync('/home/lsi/back/src/cert.pem'),
+  ca: fs.readFileSync('/home/lsi/back/src/ca.pem')
+};
 
 mongoose.connect(MONGO_CONNECTION_STRING, {
   useNewUrlParser: true,
@@ -24,8 +33,9 @@ db.on('error', () => logger.error('MongoDB connection fail:')).once(
     logger.info('Successfully connect to MongoDB');
     // await create(MOCK_USER);
     // logger.info('Successfully create mock admin user');
-    app.listen(PORT, () =>
-      logger.info(`App is running on http://localhost:${PORT}`)
-    );
+    https.createServer(sslOptions, app).listen(PORT);
+    // app.listen(PORT, () =>
+    //  logger.info(`App is running on http://localhost:${PORT}`)
+    // );
   }
 );
