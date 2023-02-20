b2a5e99377a35031f173f28baeafa9afc1957cb3
Sansrona <kingJaM00@yandex.ru>
2023-02-20 14:43:34 +0600

    fix: display critique author`s name instead of id

--- client/src/components/CustomReviewCard.vue
+++ client/src/components/CustomReviewCard.vue
@@ -1,37 +1,32 @@
 <template>
   <div class="review" :class="review.type.toLowerCase()">
     <div class="header">
-      <div>{{ review.userId }}</div>
+      <div>{{ username }}</div>
       <div>{{ date }}</div>
     </div>
     <div class="content">
       <v-expansion-panels>
-        <v-expansion-panel
-          :title="review.title || 'Коммент'"
-          :text="review.text"
-        >
+        <v-expansion-panel :title="review.title || 'Коммент'" :text="review.text">
         </v-expansion-panel>
       </v-expansion-panels>
     </div>
     <div class="footer">
-      <v-btn class="ma-2" variant="text" color="green-darken-1"
-        ><v-icon start icon="mdi-thumb-up"></v-icon>Полезно
-        {{ review.useful }}</v-btn
-      >
-      <v-btn class="ma-2" variant="text" color="red"
-        ><v-icon start icon="mdi-thumb-down"></v-icon>Нет
-        {{ review.useless }}</v-btn
-      >
+      <v-btn class="ma-2" variant="text" color="green-darken-1"><v-icon start icon="mdi-thumb-up"></v-icon>Полезно
+        {{ review.useful }}</v-btn>
+      <v-btn class="ma-2" variant="text" color="red"><v-icon start icon="mdi-thumb-down"></v-icon>Нет
+        {{ review.useless }}</v-btn>
     </div>
   </div>
 </template>
 <script setup lang="ts">
 import type { CritiqueModel } from "@/types/movies.types";
-import { computed } from "vue";
+import { computed, ref } from "vue";
+import { useAuthStore } from '@/stores/authStore'
 
 const props = defineProps<{
   review: CritiqueModel;
 }>();
+const authStore = useAuthStore();
 
 const date = computed(() => {
   const date_ = new Date(props.review.at);
@@ -48,6 +43,13 @@ const date = computed(() => {
 
   return `${reviewDate} в ${time}`;
 });
+
+const username = ref('');
+authStore.getUserById(props.review.userId).then((user) => {
+  username.value = user?.name || "Аноним"
+
+});
+
 </script>
 <style scoped>
 .review {
