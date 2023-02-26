f43f0546413145ccaf65764b0a49a10d7d9cfa19
Liavontsyeu Siarhei <leontevsi@gmail.com>
2023-02-26 22:28:16 +0300

    feat: add scroll to movies

--- client/src/components/ProfileCritiques.vue
+++ client/src/components/ProfileCritiques.vue
@@ -8,7 +8,7 @@
     <v-row> </v-row>
   </v-container>
   <v-container>
-    <v-row>
+    <v-row class="movies">
       <v-col
         v-for="(movie, index) in reviews"
         :key="index"
@@ -79,6 +79,10 @@ const reviews = computed<ProfileCritiqueModel[]>(() => {
 });
 </script>
 <style scoped>
+.movies {
+  max-height: 500px;
+  overflow: scroll;
+}
 .review {
   padding: 16px 24px;
   margin-bottom: 10px;
